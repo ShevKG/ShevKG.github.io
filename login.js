99e1ab854c50d450e0d69042469c4f006e24f999
Evgeny Shebardin <36308449+ShevKG@users.noreply.github.com>
2021-04-22 14:21:16 +0300

    minor-update-login-window
    Updated input windows.
    Added top bar menu plug for show case
    Added jQuerry function for relocation

--- login.js
+++ login.js
@@ -1,4 +1,11 @@
 
+var $user = {
+	login: "",
+	password: "",
+};
+
+var main_page_url = "login.html";
+
 
 $('body').on('click', '.password-checkbox', function(){
 
@@ -9,5 +16,32 @@ $('body').on('click', '.password-checkbox', function(){
 	} else {
 
 		$('#password-input').attr('type', 'password');
+	}
+});
+
+$('#login-input').on('keydown', function(e) {
+    if (e.which == 13) {
+			$('#password-input').focus();
+			$('#password-input').select();
+    }
+});
+
+// пока вместо обращения к таблице простая конструцкия if
+// user используется для примера
+$('#password-input').on('keydown', function(e) {
+    if (e.which == 13) {
+			console.log("user: " + $('#login-input').val())
+			console.log("password: " + $('#password-input').val())
+			if ($('#login-input').val() == "user" && $('#password-input').val() == "1234") {
+				$(location).attr('href', main_page_url);
+			}
+    }
+});
+
+$('button[name="sign-in"]').click(function () {
+	console.log("user: " + $('#login-input').val())
+	console.log("password: " + $('#password-input').val())
+	if ($('#login-input').val() == "user" && $('#password-input').val() == "1234") {
+		$(location).attr('href', main_page_url);
 	}
 });
