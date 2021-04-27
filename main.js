79fd97dc06acf6aeae10bad0f6be07e727bb5455
Evgeny Shebardin <36308449+ShevKG@users.noreply.github.com>
2021-04-27 09:35:12 +0300

    top-bar-update
    Update top-bar menu. Added scrolling events that tracking hovering zone of the menu-bar.

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-$(function() {
+/*$(function() {
   $('#menu-container').hover(function() {
     $('#')
   })
-})
+})*/
+
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+      $("#menu").css({ top: '-20%' })
+    } else {
+      $("#menu").css({ top: '0%'})
+    }
+});
+
+$("#menu-container").mouseover(function(event) {
+  $("#menu").css({ top: '0%' })
+});
+
+$("#menu-container").mouseleave(function(event) {
+  $("#menu").css( {top: '-20%'})
+});
