3a0e961eeeeeeadf83db69e66692731b94cb9197
Evgeny Shebardin <36308449+ShevKG@users.noreply.github.com>
2021-04-27 17:00:29 +0300

    main-view-changes

--- main.js
+++ main.js
@@ -4,20 +4,32 @@
   })
 })*/
 
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
+
 
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     if (scroll > 0) {
       $("#menu").css({ top: '-20%' })
+      $("#menu-container").on('mouseover', function() {
+        $("#menu").css({ top: '0%'})
+      });
+      $("#menu-container").on('mouseleave', function() {
+        $("#menu").css({ top: '-20%'})
+      });
+
     } else {
       $("#menu").css({ top: '0%'})
+        $("#menu-container").off('mouseover mouseleave');
     }
 });
 
-$("#menu-container").mouseover(function(event) {
-  $("#menu").css({ top: '0%' })
+/*$("#menu-container").mouseover(function(event) {
+    $("#menu").css({ top: '0%' })
 });
 
 $("#menu-container").mouseleave(function(event) {
-  $("#menu").css( {top: '-20%'})
-});
+  ("#menu").css( {top: '-20%'})
+});*/
